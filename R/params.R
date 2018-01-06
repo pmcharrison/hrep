1f7a1ca450aa28099d7885a8a37adbbdbeb13146
Peter M C Harrison <pmc.harrison@gmail.com>
2018-01-06 12:33:53 +0000

    Adding get_midi_params

---
+++ R/params.R
@@ -0,0 +1,18 @@
+#' @export
+get_midi_params <- function(
+  stretched_octave = FALSE,
+  tuning_ref_Hz = 440,
+  num_harmonics = 11, # including the fundamental
+  roll_off = 1, # equivalent to Milne's rho, except for level rather than loudness; levels are multiplied by 1 / (n ^ roll_off)
+  frequency_digits = 6,
+  fundamental_level_dB = 60 # not used in hutch, seth, or vass
+) {
+  list(
+    stretched_octave = stretched_octave,
+    tuning_ref_Hz = tuning_ref_Hz,
+    num_harmonics = num_harmonics,
+    roll_off = roll_off,
+    frequency_digits = frequency_digits,
+    fundamental_level_dB = fundamental_level_dB
+  )
+}
