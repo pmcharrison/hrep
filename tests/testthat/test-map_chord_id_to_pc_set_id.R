191901be46857140af8e074faab43bc4163b8e49
Peter M C Harrison <pmc.harrison@gmail.com>
2018-01-24 09:52:02 +0200

    Adding tests for map_chord_id_to_pc_set_id

--- tests/testthat/test-map_chord_id_to_pc_set_id.R
+++ tests/testthat/test-map_chord_id_to_pc_set_id.R
@@ -1,9 +1,24 @@
 context("map_chord_id_to_pc_set_id")
 
+library(magrittr)
+
 test_that("Format", {
   chord_ids <- c(3, 56, 80)
   pc_set_ids <- map_chord_id_to_pc_set_id(chord_ids)
   expect_is(pc_set_ids, "integer")
   expect_equal(length(chord_ids),
                length(pc_set_ids))
+})
+
+test_that("Example results", {
+  expect_equal(
+    list(c(48, 64, 67), c(48, 63, 67)) %>%
+      encode_chords %>%
+      map_chord_id_to_pc_set_id %>%
+      decode_pc_sets,
+    list(
+      c(0, 4, 7),
+      c(0, 3, 7)
+    )
+  )
 })
