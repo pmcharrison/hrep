e7b072719d40f696f56c179b27e17755f3962096
Peter M C Harrison <pmc.harrison@gmail.com>
2018-02-01 11:50:02 +0000

    extra tests

---
+++ tests/testthat/test-convert_pc_set_to_pc_spectrum.R
@@ -0,0 +1,16 @@
+context("convert_pc_set_to_pc_spectrum")
+
+test_that("Compare results to saved results from prior code versions", {
+  expect_equal(
+    convert_pc_set_to_pc_spectrum(c(0, 4, 7)),
+    readRDS("pc_spectrum_example_outputs/0-4-7.rds")
+  )
+  expect_equal(
+    convert_pc_set_to_pc_spectrum(c(2, 5, 9)),
+    readRDS("pc_spectrum_example_outputs/2-5-9.rds")
+  )
+  expect_equal(
+    convert_pc_set_to_pc_spectrum(c(1, 3, 2)),
+    readRDS("pc_spectrum_example_outputs/1-3-2.rds")
+  )
+})
