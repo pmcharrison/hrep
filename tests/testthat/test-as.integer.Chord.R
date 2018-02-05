045ca32cdf9a5f4e0b9a25b7aefd2aaded0b254f
Peter M C Harrison <pmc.harrison@gmail.com>
2018-02-05 16:12:33 +0000

    Adding test for as.integer

---
+++ tests/testthat/test-as.integer.Chord.R
@@ -0,0 +1,8 @@
+context("as.integer.Chord")
+
+test_that("example", {
+  chord <- make_chord(1, c(3, 6, 7))
+  res <- as.integer(chord)
+  expect_is(res, "integer")
+  expect_equal(res, c(49, 63, 66, 67))
+})
