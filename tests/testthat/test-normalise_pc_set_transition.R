86897ed20e9fd9833e79b47f9a82006cef5a8acb
Peter M C Harrison <pmc.harrison@gmail.com>
2018-05-27 10:10:53 +0100

    Fixing test

--- tests/testthat/test-normalise_pc_set_transition.R
+++ tests/testthat/test-normalise_pc_set_transition.R
@@ -8,12 +8,12 @@ library(magrittr)
 
 test_that("perfect cadence", {
   trans <- c(
-    c(0, 4, 7) %>% new_pc_set %>% add(10) %>% encode_pc_set,
-    c(0, 4, 7) %>% new_pc_set %>% add(3) %>% encode_pc_set
+    c(0, 4, 7) %>% new_pc_set %>% transpose(10) %>% encode_pc_set,
+    c(0, 4, 7) %>% new_pc_set %>% transpose(3) %>% encode_pc_set
   )
   norm <- c(
-    c(0, 4, 7) %>% new_pc_set %>% add(7) %>% encode_pc_set,
-    c(0, 4, 7) %>% new_pc_set %>% add(0) %>% encode_pc_set
+    c(0, 4, 7) %>% new_pc_set %>% transpose(0) %>% encode_pc_set,
+    c(0, 4, 7) %>% new_pc_set %>% transpose(-7) %>% encode_pc_set
   )
   expect_equal(
     normalise_pc_set_transition(trans[1], trans[2]),
