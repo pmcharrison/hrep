9301e2e02fa937ed79a7892908337f5dea67199b
Peter M C Harrison <pmc.harrison@gmail.com>
2018-11-28 16:42:36 +0000

    Typo fix

--- tests/testthat/test-tp.R
+++ tests/testthat/test-tp.R
@@ -1,4 +1,4 @@
-context("tpos")
+context("tp")
 
 library(magrittr)
 
@@ -20,21 +20,21 @@ test_that("reversible", {
   for (i in seq_len(10)) {
     c1 <- pc_chord(sample(11, 5))
     int <- sample(-11:11, 1)
-    c2 <- tpos(c1, int)
-    c3 <- tpos(c2, -int)
+    c2 <- tp(c1, int)
+    c3 <- tp(c2, -int)
     expect_equal(c1, c3)
   }
 })
 
 test_that("pc_set", {
   p1 <- pc_set(c(0, 4, 7))
-  expect_is(tpos(p1, 2), "pc_set")
+  expect_is(tp(p1, 2), "pc_set")
   expect_equal(
-    tpos(p1, 2) %>% as.integer,
+    tp(p1, 2) %>% as.integer,
     c(2, 6, 9)
   )
   expect_equal(
-    c(0, 4, 7) %>% pc_set %>% tpos(-2) %>% as.integer,
+    c(0, 4, 7) %>% pc_set %>% tp(-2) %>% as.integer,
     c(2, 5, 10)
   )
 })
