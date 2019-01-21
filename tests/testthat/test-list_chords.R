66ffc2061302c9edf59a40e2da76febfaea6f541
Peter M C Harrison <pmc.harrison@gmail.com>
2019-01-21 16:22:54 +0000

    Fixing tests

--- tests/testthat/test-list_chords.R
+++ tests/testthat/test-list_chords.R
@@ -3,7 +3,7 @@ context("test-list_chords")
 test_that("examples", {
   expect_equal(list_chords("pc_set"),
                pc_set_alphabet$by_id)
-  expect_equal(list_chords("pc_chord_type") %>% map(as.integer),
+  expect_equal(list_chords("pc_chord_type") %>% lapply(as.integer),
                pc_chord_alphabet$by_id[seq_len(alphabet_size("pc_chord") / 12L)] %>%
-                 map(as.integer))
+                 lapply(as.integer))
 })
