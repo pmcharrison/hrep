7eb7ddbc0cc9aafa4882bb72e2307d82dcfe4816
Peter M C Harrison <pmc.harrison@gmail.com>
2019-06-19 21:59:40 +0100

    Prototyping new alphabet functions

---
+++ scratch.R
@@ -0,0 +1,39 @@
+# Order-insensitive
+encode_pc_set <- function(pc_set) {
+  sum((2 ^ (11:0)) * (0:11 %in% pc_set))
+}
+
+decode_pc_set <- function(x) {
+  if (!is.numeric(x) || length(x) != 1 || x < 1 || x > 4095)
+    stop("invalid pc_set id")
+  x <- as.integer(x)
+  binary <- rev(intToBits(x)[1:12] == 1)
+  (0:11)[binary]
+}
+
+# Order-insensitive, little error checking
+encode_pc_chord_type <- function(pc_chord_type) {
+  if (length(pc_chord_type) == 0) stop("invalid pc_chord_type")
+  1L + sum((2L ^ (10:0)) * (1:11 %in% pc_chord_type))
+}
+
+decode_pc_chord_type <- function(x) {
+  if (!is.numeric(x) || length(x) != 1 || x < 1 || x > 2047)
+    stop("invalid pc_chord_type id")
+  x <- as.integer(x)
+  binary <- rev(intToBits(x - 1L)[1:11] == 1)
+  c(0L, (1:11)[binary])
+}
+
+encode_pc_chord <- function(pc_chord) {
+  bass <- pc_chord[1]
+  chord_type <- (pc_chord - bass) %% 12L
+  as.integer(2048L * bass + encode_pc_chord_type(chord_type))
+}
+
+decode_pc_chord <- function(x) {
+  x <- as.integer(x)
+  bass <- x %/% 2048L
+  chord_type <- decode_pc_chord_type(x %% 2048L)
+  c(bass, sort.int((chord_type[-1L] + bass) %% 12L))
+}
