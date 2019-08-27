ffa5679041004c450605f10047e898d2fcb120f7
Peter M C Harrison <pmc.harrison@gmail.com>
2019-08-27 10:54:13 +0100

    Documenting chord qualities

--- R/chord-qualities.R
+++ R/chord-qualities.R
@@ -1,3 +1,35 @@
+#' Get chord qualities
+#'
+#' Returns a \code{\link[tibble]{tibble}} of chord qualities and different
+#' textual labels for these chord qualities.
+#' In practice most users will access this information through the
+#' function \code{\link{decode_chord_quality}}.
+#'
+#' The \code{tibble} has one row for each unique chord quality,
+#' where a chord quality is defined as a set of pitch classes
+#' expressed relative to the chord root.
+#'
+#' - The \code{pitch_classes} column provides the chord quality as an integer vector,
+#' corresponding to a list of pitch classes expressed relative to the chord root.
+#' - The \code{scale_degrees} column lists the scale degrees present in the
+#' chord quality in a more traditional human-readable format.
+#' - The \code{description} column provides a textual description for the
+#' chord quality.
+#' - The remaining columns are prefixed by \code{key_},
+#' and provide textual abbreviations for the chord quality.
+#' These \code{key_} elements define the legal inputs for the function
+#' \code{\link{decode_chord_quality}}.
+#'
+#' The data is read from the package file 'inst/chord-qualities.csv'.
+#' Additions can be proposed by GitHub: \url{https://github.com/pmcharrison/hrep}.
+#'
+#' @seealso
+#' - \code{\link{decode_chord_quality}} for decoding chord qualities.
+#' - \code{\link{register_chord_quality}} for registering additional chord qualities.
+#'
+#' @md
+#'
+#' @export
 get_chord_qualities_df <- function() {
   file <- system.file("chord-qualities.csv", package = "hrep", mustWork = TRUE)
 
@@ -36,21 +68,61 @@ get_chord_qualities_df <- function() {
 }
 
 assert_legal_pc_set <- function(x) {
-  if (anyDuplicated(x) || !checkmate::qtest(x, "X+[0,12)"))
+  if (anyDuplicated(x) || !checkmate::qtest(x, "X+[0,12)") ||
+      !all(x == sort(x)))
     stop("invalid pitch-class set: ",
          paste(x, collapse = " "))
 }
 
 CHORD_QUALITIES <- new.env()
 
-register_chord_quality <- function(key, value) {
+#' Register chord quality
+#'
+#' This function registers a new chord quality in the database
+#' used by the \code{\link{decode_chord_quality}} function.
+#' Unless overwritten,
+#' this chord quality will persist for the remainder of the R session.
+#'
+#' @param key
+#' (Character scalar) Textual key identifying the chord quality.
+#'
+#' @param value
+#' (Integer vector) Chord quality,
+#' provided as an integer vector of non-duplicated pitch classes
+#' in ascending order.
+#'
+#' @param overwrite
+#' (Logical scalar) If \code{FALSE}, an error will
+#' be thrown when trying to redefine a pre-existing key.
+#'
+#' @examples
+#' decode_chord_quality("my_chord") # returns NULL
+#' register_chord_quality("my_chord", c(0, 1, 2))
+#' decode_chord_quality("my_chord") # returns c(0, 1, 2)
+#'
+#' @seealso
+#' - \code{\link{decode_chord_quality}} for accessing the database.
+#' - \code{\link{initialise_chord_qualities}} for resetting the database.
+#'
+#' @md
+#'
+#' @export
+register_chord_quality <- function(key, value, overwrite = FALSE) {
   checkmate::qassert(key, "S1")
+  checkmate::qassert(overwrite, "B1")
   assert_legal_pc_set(value)
-  if (!is.null(CHORD_QUALITIES[[key]]))
+  if (!is.null(CHORD_QUALITIES[[key]]) && !overwrite)
     stop("cannot insert duplicate chord quality: ", key)
-  CHORD_QUALITIES[[key]] <- value
+  CHORD_QUALITIES[[key]] <- as.integer(value)
 }
 
+#' Initialise chord qualities database
+#'
+#' (Re)initialises the chord qualities used by the \code{\link{decode_chord_quality}}
+#' function to values specified by the \code{\link{get_chord_qualities_df}}
+#' function.
+#'
+#' @export
 initialise_chord_qualities <- function() {
   df <- get_chord_qualities_df()
   which_keys <- attr(df, "which_keys")
@@ -61,6 +133,36 @@ initialise_chord_qualities <- function() {
   }
 }
 
+#' Decode chord quality
+#'
+#' Decodes a given textual label into the implied chord quality.
+#'
+#' No preprocessing is applied to \code{x}.
+#'
+#' @param x
+#' (Character scalar)
+#' Label to decode.
+#'
+#' @param must_work
+#' (Logical scalar)
+#' If \code{TRUE}, the function will throw an error if the textual label
+#' is not recognised.
+#'
+#' @param return
+#' If the textual label is recognised,
+#' returns an integer vector corresponding to the pitch-class set
+#' expressed relative to the chord root,
+#' otherwise returns \code{NULL}
+#' (or an error if \code{must_work} is \code{TRUE}).
+#'
+#' @seealso
+#' - \code{\link{register_chord_quality}} for registering additional chord qualities
+#' in the database.
+#' - \code{\link{initialise_chord_qualities}} for resetting the database.
+#'
+#' @md
+#'
+#' @export
 decode_chord_quality <- function(x, must_work = FALSE) {
   checkmate::qassert(x, "S1")
   checkmate::qassert(must_work, "B1")
