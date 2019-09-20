f1795d1028e4c31ec613196a45af29355c3de722
Peter M C Harrison <pmc.harrison@gmail.com>
2019-09-20 15:56:52 +0100

    Updated onLoad message

--- R/on-load.R
+++ R/on-load.R
@@ -4,8 +4,8 @@
   packageStartupMessage(
     "Version 0.10.0.9001 of the hrep package redefined the ",
     "integer encodings for pc_chord, pc_chord_type, pc_set, and pc_set_type. ",
-    "The packages hcorp, incon/corpdiss, and voicer have been updated ",
-    "correspondingly. Please make sure that you have installed the latest ",
-    "versions of these packages."
+    "The packages hcorp, incon/corpdiss/parn88, and voicer have been updated ",
+    "correspondingly. If you installed any of these packages before ",
+    "20th September 2019, please reinstall them."
   )
 }
