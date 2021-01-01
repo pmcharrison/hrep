6cdf9626811679219adea11fb0e641199a4afd0e
Peter Harrison <pmc.harrison@gmail.com>
2021-01-01 22:28:27 +0000

    Add base and peak to filter_spectrum_gaussian

--- R/spectral-filter.R
+++ R/spectral-filter.R
@@ -6,7 +6,8 @@
 #'
 #' @param fun Function to apply. The input should be a vector of frequencies,
 #' the output should be a vector of envelope amplitudes for those frequencies.
-#' An amplitude of 1 means that the spectrum is left unchanged at that location.
+#' The spectral amplitudes will be multiplied by the outputs of this function
+#' for each frequency.
 #'
 #' @param ... Optional parameters passed to \code{\link{sparse_fr_spectrum}}.
 #'
@@ -37,19 +38,37 @@ filter_spectrum.sparse_fr_spectrum <- function(x, fun, ...) {
 #' Vector of widths (i.e. SDs) for the Gaussians.
 #' If only one number is provided, this same number is used for all Gaussians.
 #'
+#' @param base
+#' (Numeric scalar, default = 0)
+#' Baseline upon which the Gaussian sits.
+#' If this is set to 1, then partials outside the range of the Gaussian
+#' will be unaffected by the filter; if this is left at 0,
+#' then these partials will be removed.
+#'
+#' @param peak
+#' (Numeric scalar, default = 0)
+#' Notional peak of the Gaussian.
+#'
 #' @inheritParams filter_spectrum
 #'
 #' @export
-filter_spectrum_gaussian <- function(x, location, width = 50, ...) {
+filter_spectrum_gaussian <- function(x,
+                                     location,
+                                     width = 50,
+                                     base = 0,
+                                     peak = 1,
+                                     ...) {
   stopifnot(length(location) > 0,
-            length(width) == 1 || length(width) == length(location))
+            length(width) == 1 || length(width) == length(location),
+            length(base) == 1,
+            length(peak) == 1,
+            base <= peak)
   if (length(width) == 1) {
     width <- rep(width, times = length(location))
   }
   f <- function(x) {
     purrr::map2(location, width, function(.location, .width) {
-      dens <- stats::dnorm(x, mean = .location, sd = .width)
-      dens / max(dens)
+      base + (peak - base) * exp(- (x - .location) ^ 2 / (2 * .width ^ 2))
     }) %>%
       do.call(rbind, .) %>%
       colSums()
